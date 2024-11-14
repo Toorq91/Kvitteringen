6dbc679f1927e533ee394aca61c35028827e1ee0
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-11-14 14:57:59 +0100

    x

--- src/app/dagligvarer/page.js
+++ src/app/dagligvarer/page.js
@@ -2,7 +2,7 @@ export default function DagligvarePage() {
     return (
         <div className="container">
         <h1>Velkommen til Dagligvare siden!</h1>
-        <p>Her var det tomt enn så lenge xD</p>
+        <p>Her var det tomt enn så lenge :)</p>
         </div>
     );
 }
